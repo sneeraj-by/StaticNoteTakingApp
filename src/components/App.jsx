a18982762014f217a947f72de73304878e32a11d
sneeraj-by <108170565+sneeraj-by@users.noreply.github.com>
2022-07-12 20:00:05 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Note Taking App</h1>
+      <p>
+        This is Note Taking App build with JavaScript and React JS. You can add
+        more notes, but this is a static version at the moment.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
